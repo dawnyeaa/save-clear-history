a2287de6b181d279514f2dd671b5ec74d7193e29
dawn <hello.tfanning@gmail.com>
2022-06-09 18:17:12 +1000

    now uses config file for settings, first setting is delAfterSave
    Creates a config file in install directory, for human readable settings.
    
    Only setting used is delAfterSave which determines whether or not to use the default process of deleting after the save (to keep history in the saved file) or before (to keep the history clean in the saved file)

--- save-clear-history.py
+++ save-clear-history.py
@@ -1,10 +1,46 @@
+from os.path import exists
+import configparser
 import maya.mel as mel
 import maya.OpenMaya as om
 import maya.cmds as cmds
 
+# parses string values to be able to cast to booleans (True or False)
+def truey(value, default=False):
+    result = default
+    if value.lower() in ['true', '1', 't', 'y', 'yes']:
+        result = True
+    elif value.lower() in ['false', '0', 'f', 'n', 'no']:
+        result = False
+    return result
+
+# setup the parser to be able to read the config file
+config = configparser.ConfigParser()
+config.optionxform=str
+
+# saveClearHistory install directory located with config.ini within
 dir = cmds.internalVar(usd=True) + 'modules/saveClearHistory/config.ini'
 
+# if the config.ini does not exist, create it with the default values
+if not exists(dir):
+    # default behaviour for plugin is to delete history AFTER save, to preserve history in saved files
+    config['DEFAULT']['deleteAfterSave'] = 'true'
+    with open(dir, 'w') as configfile:
+        config.write(configfile)
+        
+# read the config file, and put clean data in relevant variables
+config.read(dir)
+delAfter = truey(config['DEFAULT']['deleteAfterSave'], True)
+
+# callback function called upon save action in maya (see below for before or after save)
 def handle_save(*args):
+  # all non deformer history is deleted
   mel.eval("BakeAllNonDefHistory;")
 
-id = om.MSceneMessage.addCallback(om.MSceneMessage.kAfterSave, handle_save)
+# decides whether to setup callback for before or after save, based on config setting
+if delAfter:
+  message = om.MSceneMessage.kAfterSave
+else:
+  message = om.MSceneMessage.kBeforeSave
+
+# create callback for the handle_save function with the appropriate callback trigger (message)
+id = om.MSceneMessage.addCallback(message, handle_save)
